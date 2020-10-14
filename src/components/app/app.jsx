5fdb0d33b9cba6b2888e16dcaafc3aeae96f6050
Yura <yurii_kulachenko@mail.ru>
2020-10-14 20:39:14 +0300

    удаляет служебные маршруты

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,15 +2,12 @@ import React from 'react';
 import WelcomeScreen from '../welcome-screen/welcome-screen';
 import PropTypes from 'prop-types';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
-import ArtistQuestionScreen from '../artist-question-screen/artist-question-screen';
 import AuthScreen from '../auth-screen/auth-screen';
 import GameOverScreen from '../game-over-screen/game-over-screen';
-import GenreQuestionScreen from '../genre-question-screen/genre-question-screen';
 import WinScreen from '../win-screen/win-screen';
 import GameScreen from '../game-screen/game-screen';
 
 const App = ({errorsCount, questions}) => {
-  const [firstQuestion, secondQuestion] = questions;
 
   return (
     <BrowserRouter>
@@ -32,18 +29,6 @@ const App = ({errorsCount, questions}) => {
         </Route>
         <Route exact path="/lose">
           <GameOverScreen />
-        </Route>
-        <Route exact path="/dev-artist">
-          <ArtistQuestionScreen
-            question={secondQuestion}
-            onAnswer={() => {}}
-          />
-        </Route>
-        <Route exact path="/dev-genre">
-          <GenreQuestionScreen
-            question={firstQuestion}
-            onAnswer={() => {}}
-          />
         </Route>
         <Route exact path="/game">
           <GameScreen
